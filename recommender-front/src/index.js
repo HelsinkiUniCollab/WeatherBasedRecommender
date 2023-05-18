9266f3c22416c2a3260416357f9cfc6cc829380e
Joni Joensuu <joni.joens@gmail.com>
2023-05-18 18:13:25 +0300

    removed unnecessary lines

--- recommender-front/src/index.js
+++ recommender-front/src/index.js
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-<link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
-integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
-crossorigin=""/>
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
