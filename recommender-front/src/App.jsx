fada69f89f000dbb16bb6a4df663dd611902108a
ruusukivi <ruusukivi@gmail.com>
2023-06-04 18:44:51 +0300

    Added HelmetProvider for metadata

--- recommender-front/src/App.jsx
+++ recommender-front/src/App.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 import MapComponent from './components/MapComponent';
 import 'leaflet/dist/leaflet.css';
 
 function App() {
   return (
-    <center>
-      <h1>
-        Weather-Based Recommender
-      </h1>
+    <HelmetProvider>
       <div>
-        <MapComponent />
+        <Helmet>
+          <title> Weather-Based Recommender</title>
+          <meta name="description" content="Weather-Based Recommender" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+        </Helmet>
+        <center>
+          <h1>
+            Weather-Based Recommender
+          </h1>
+          <MapComponent />
+        </center>
       </div>
-    </center>
+    </HelmetProvider>
   );
 }
 
