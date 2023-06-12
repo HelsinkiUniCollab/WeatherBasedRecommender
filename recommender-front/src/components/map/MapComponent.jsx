9f81e8c735e1c36539acb2551fe1999585da797a
isakpulkki <isak@pulkki.me>
2023-06-13 00:28:08 +0300

    Pass POI data and hour data

--- recommender-front/src/components/map/MapComponent.jsx
+++ recommender-front/src/components/map/MapComponent.jsx
@@ -1,25 +1,10 @@
+import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
-import React, { useEffect, useState } from 'react';
 import MarkersComponent from './MarkersComponent';
 import '../../assets/style.css';
 
-function MapComponent({ accessibility }) {
+function MapComponent({ poiData, forecastIndex }) {
   const position = [60.2049, 24.9649];
-  const [poiData, setPoiData] = useState([]);
-
-  useEffect(() => {
-    async function fetchPoiData() {
-      try {
-        const apiUrl = process.env.REACT_APP_BACKEND_URL || 'http://localhost:5000';
-        const response = await fetch(`${apiUrl}/api/poi/${accessibility}`);
-        const data = await response.json();
-        setPoiData(data);
-      } catch (error) {
-        console.error('Error fetching POI data:', error);
-      }
-    }
-    fetchPoiData();
-  }, [accessibility]);
 
   return (
     <MapContainer id="map" center={position} zoom={14} scrollWheelZoom={false}>
@@ -27,7 +12,7 @@ function MapComponent({ accessibility }) {
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <MarkersComponent poiData={poiData} />
+      <MarkersComponent poiData={poiData} forecast={forecastIndex} />
     </MapContainer>
   );
 }
