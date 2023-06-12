9415c2db4588cea0eb7ed44dda2fb0625a0c0c78
isakpulkki <isak@pulkki.me>
2023-06-12 20:28:08 +0300

    Forecast data toi POIs

--- recommender-back/src/apis/poi.py
+++ recommender-back/src/apis/poi.py
@@ -1,11 +1,21 @@
 import json
 from apis import weather
 from apis import helpers
+import requests
+import json
 
 
 #Todo: give category as a parameter to get more accurate data.
 
-def get_pois_as_json(accessibility = False, category=None):
+def get_pois(category=None):
+    if category is None:
+        category = ['open_air_water', 'fitness_parks']
+    paths = [
+        f"src/apis/poi_data/sports_and_physical/water_sports/{category[0]}.json",
+        f"src/apis/poi_data/sports_and_physical/outdoor_sports/neighborhood_sports/{category[1]}.json"
+        ]
+    return merge_json(paths)
+def get_pois_as_json(accessibility = False, time=None):
     """
     Retrieves points of interest (POIs) from a JSON file and enriches them with current weather data.
 
@@ -16,31 +26,53 @@ def get_pois_as_json(accessibility = False, category=None):
         KeyError: If an error occurs while processing the data.
 
     """
-    if category is None:
-        category = ['open_air_water', 'fitness_parks']
-    paths = [
-        f"src/apis/poi_data/sports_and_physical/water_sports/{category[0]}.json",
-        f"src/apis/poi_data/sports_and_physical/outdoor_sports/neighborhood_sports/{category[1]}.json"
-        ]
     try:
-        data = merge_json(paths)
+        data = get_pois()
+
+        print("success")
         weatherdata = weather.get_current_weather()
+        url = 'http://127.0.0.1:5000/api/forecast'  # Replace with the desired website URL
+        response = requests.get(url)
+        print("success")
+        forecastdata = response.json()
         updated_data = []
         for item in data:
-            item = find_nearest_stations_weather_data(weatherdata, item)
+            item = find_nearest_stations_weather_data(weatherdata, forecastdata, item)
+            print("success")
             if accessibility not in item["accessibility_shortcoming_count"]:
                 updated_data.append(item)
-            item = add_score_to_poi(item)
         return json.dumps(updated_data)
     except KeyError as error:
         return {
             'message': 'An error occurred',
             'status': 500,
-            'error': str(error),
+            'error': str(error)
         }
+    
+def get_closest_poi_coordinates_data(coordinates, data):
+    returned_data = {}
+    for hour in data:
+        returned_data[hour] = {}
+    pois = get_pois()
+    closest_coordinates = {}
+    for poi in pois:
+        smallest, nearest = float('inf'), ''
+        lat = float(poi['location']['coordinates'][1])
+        lon = float(poi['location']['coordinates'][0])
+        for coordinate in coordinates:
+            dist = abs(coordinate[0] - lon)\
+            + abs(coordinate[1] - lat)
+            if dist < smallest:
+                smallest, nearest = dist, coordinate
+        closest_coordinates[(f"({nearest[0]}, {nearest[1]})")] = f"{lat}, {lon}"
+        for hour in data:
+            for key, value in closest_coordinates.items():
+                fore = data[hour][key]
+                returned_data[hour][f"{value}"] = weather.parse_forecast(fore)
+    return returned_data
 
 
-def find_nearest_stations_weather_data(weatherdata, item):
+def find_nearest_stations_weather_data(weatherdata, forecastdata, item):
     """
     Finds the nearest weather station to a given POI and adds its weather data to the POI.
 
@@ -60,7 +92,11 @@ def find_nearest_stations_weather_data(weatherdata, item):
             + abs(weatherdata[station]['Latitude'] - lat)
         if dist < smallest:
             smallest, nearest = dist, station
-    item['weather'] = weatherdata[nearest]
+    item['weather'] = {}
+    item['weather']["current"] = weatherdata[nearest]
+    for hour in forecastdata:
+        data = forecastdata[hour]
+        item["weather"][f'{hour[11:16]}'] = data[f"{lat}, {lon}"]
     return item
 
 def merge_json(paths):
