b65445e4875d124701bc0f3da95842e6fdd95023
isakpulkki <isak@pulkki.me>
2023-06-13 00:28:38 +0300

    Render only hour data

--- recommender-front/src/components/map/MarkersComponent.jsx
+++ recommender-front/src/components/map/MarkersComponent.jsx
@@ -2,33 +2,36 @@ import React from 'react';
 import { Marker, Popup } from 'react-leaflet';
 import createMarkerIcon from '../../utils/Icon';
 
-function MarkersComponent({ poiData }) {
-  return poiData.map((poi) => {
-    const tags = Object.entries(poi.weather);
-    const markerIcon = createMarkerIcon(poi.score);
-    return (
-      <Marker
-        position={[poi.location.coordinates[1], poi.location.coordinates[0]]}
-        key={poi.id}
-        icon={markerIcon}
-      >
-        <Popup>
-          <h2>{poi.name.fi}</h2>
-          <ul>
-            {tags.map(([key, value]) => (
-              key !== 'Longitude' && key !== 'Latitude' && (
+function MarkersComponent({ poiData, forecast }) {
+  if (poiData && forecast) {
+    return poiData.map((poi) => {
+      const tags = Object.entries(poi.weather[forecast]);
+      const markerIcon = createMarkerIcon(poi.weather[forecast].score);
+      return (
+        <Marker
+          position={[poi.location.coordinates[1], poi.location.coordinates[0]]}
+          key={poi.id}
+          icon={markerIcon}
+        >
+          <Popup>
+            <h2>{poi.name.fi}</h2>
+            <ul>
+              {tags.map(([key, value]) => (
+                key !== 'Longitude' && key !== 'Latitude' && key !== 'score' && (
                 <li key={key}>
                   <strong>{key}</strong>
                   :
+                  {' '}
                   {value}
                 </li>
-              )
-            ))}
-          </ul>
-        </Popup>
-      </Marker>
-    );
-  });
+                )
+              ))}
+            </ul>
+          </Popup>
+        </Marker>
+      );
+    });
+  }
 }
 
 export default MarkersComponent;
