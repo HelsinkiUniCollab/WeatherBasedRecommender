dea41b91ea675c94451f12f3493a344a46c1251f
ruusukivi <ruusukivi@gmail.com>
2023-06-16 17:02:08 +0300

    Improved slider

--- recommender-front/src/components/header/HeaderComponent.jsx
+++ recommender-front/src/components/header/HeaderComponent.jsx
@@ -5,10 +5,15 @@ import Select from '@mui/material/Select';
 import Slider from '@mui/material/Slider';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import useMediaQuery from '@mui/material/useMediaQuery';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { useTheme } from '@mui/system';
 
 function HeaderComponent({
   accessibility, handleChange, times, sliderValue, onChange,
 }) {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('md'));
   const hours = [];
   if (times) {
     for (let i = 0; i <= times.length; i += 1) {
@@ -24,11 +29,12 @@ function HeaderComponent({
       justifyContent="center"
       alignItems="center"
       my={1}
+      key="main"
     >
-      <Grid item xs={5} sm={5} md={3} lg={3}>
+      <Grid item xs={5} sm={5} md={3} lg={3} key="title">
         <Typography variant="h1">Weather-Based Recommender</Typography>
       </Grid>
-      <Grid item xs={7} sm={7} md={3} lg={3} className="dropdown-item">
+      <Grid item xs={7} sm={7} md={3} lg={3} className="dropdown-item" key="dropdown">
         <FormControl>
           <Select displayEmpty value={accessibility} onChange={handleChange}>
             <MenuItem value="">
@@ -52,18 +58,30 @@ function HeaderComponent({
           </Select>
         </FormControl>
       </Grid>
-      <Grid item xs={12} sm={12} md={12} lg={12} className="slider-item">
-        <Typography variant="h2">Time</Typography>
-        <Slider
-          value={sliderValue}
-          onChange={onChange}
-          min={0}
-          max={24}
-          marks={hours}
-        />
-      </Grid>
+      {isMobile ? (
+        <Grid item xs={11} sm={11} className="slider-item" key="slider-mobile">
+          <Typography variant="h2">Time</Typography>
+          <Slider
+            value={sliderValue}
+            onChange={onChange}
+            min={0}
+            max={8}
+            marks={hours}
+          />
+        </Grid>
+      ) : (
+        <Grid item xs={11} sm={11} md={10} lg={10} className="slider-item" key="slider">
+          <Typography variant="h2">Time</Typography>
+          <Slider
+            value={sliderValue}
+            onChange={onChange}
+            min={0}
+            max={24}
+            marks={hours}
+          />
+        </Grid>
+      )}
     </Grid>
   );
 }
-
 export default HeaderComponent;
