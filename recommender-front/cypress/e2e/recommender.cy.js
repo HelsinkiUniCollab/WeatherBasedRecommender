c5c0110314e27371a1da3520949e639fd5c60333
Joni Joensuu <joni.joens@gmail.com>
2023-06-18 19:31:15 +0300

    cypress-tests

---
+++ recommender-front/cypress/e2e/recommender.cy.js
@@ -0,0 +1,56 @@
+import mockPOIS from '../mockData';
+
+describe('Map and POI features', () => {
+  beforeEach(() => {
+    cy.intercept('GET', 'http://localhost:5000/api/poi/', mockPOIS);
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('should display the MapContainer on initial load', () => {
+    cy.get('.leaflet-container').should('be.visible');
+  });
+
+  it('should display all POI markers on initial load', () => {
+    cy.get('.leaflet-marker-icon')
+      .should('have.length', mockPOIS.length);
+  });
+
+  it('should display clustered markers correctly', () => {
+    // Create a copy of the mock POI data
+    const clusteredMockPOIs = JSON.parse(JSON.stringify(mockPOIS));
+    // Move one POI closer to another so they are clustered
+    clusteredMockPOIs[1].location.coordinates = [24.952, 60.171];
+    cy.intercept('GET', 'http://localhost:5000/api/poi/', clusteredMockPOIs);
+    cy.visit('http://localhost:3000/');
+
+    cy.get('.leaflet-marker-icon')
+      .should('have.length', mockPOIS.length - 1);
+  });
+
+  it('should update the popup content when the time slider is moved', () => {
+    cy.get('.leaflet-marker-icon').first().click();
+
+    cy.get('.leaflet-popup-content')
+      .find('ul li')
+      .should(($lis) => {
+        expect($lis.eq(0)).to.contain('20.0 °C');
+      });
+
+    cy.get('.MuiSlider-root').click(50, 0);
+
+    cy.get('.leaflet-marker-icon').first().click();
+
+    cy.get('.leaflet-popup-content')
+      .find('ul li')
+      .should(($lis) => {
+        expect($lis.eq(0)).to.contain('-5.5 °C');
+      });
+  });
+
+  it('should update amount of markers when accessibility is selected', () => {
+    const wheelChairMockData = mockPOIS.slice(-2);
+    cy.intercept('GET', 'http://localhost:5000/api/poi/wheelchair', wheelChairMockData);
+    cy.get('.MuiSelect-select').click();
+    cy.get('[data-value="wheelchair"]').click();
+  });
+});
