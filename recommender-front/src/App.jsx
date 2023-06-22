95820f6581c8e9a08d58146a715b88f349c24018
ruusukivi <ruusukivi@gmail.com>
2023-06-22 17:14:58 +0300

    Fixed minor error

--- recommender-front/src/App.jsx
+++ recommender-front/src/App.jsx
@@ -68,7 +68,7 @@ function App() {
   useEffect(() => {
     const weather = weatherData;
     const windSpeed = weather?.['Wind speed']?.value;
-    setShowAlert(windSpeed <= 17);
+    setShowAlert(windSpeed >= 17);
   }, [weatherData]);
 
   return (
