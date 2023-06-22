af0944b0ddd448454f87d76d462d3809cac99e2c
ruusukivi <ruusukivi@gmail.com>
2023-06-22 19:05:47 +0300

    Reverted some changes

--- recommender-front/cypress/e2e/recommender.cy.js
+++ recommender-front/cypress/e2e/recommender.cy.js
@@ -2,8 +2,7 @@ import mockPOIS from '../mockData';
 
 describe('Map and POI features', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'http://localhost:5000/api/poi/', mockPOIS).as('getPOIs');
-    cy.wait('@getPOIs');
+    cy.intercept('GET', 'http://localhost:5000/api/poi/', mockPOIS);
     cy.visit('');
   });
 
