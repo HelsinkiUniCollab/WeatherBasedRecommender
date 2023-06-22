f8d0febbf22025c5560e59dde807144010fe83e7
isakpulkki <isak@pulkki.me>
2023-06-23 00:53:57 +0300

    Add info icon

--- recommender-front/src/components/header/InfoComponent.jsx
+++ recommender-front/src/components/header/InfoComponent.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Button, Dialog, DialogContent, DialogTitle } from '@mui/material';
+import InfoIcon from '@mui/icons-material/Info';
 import Typography from '@mui/material/Typography';
 
 function InfoComponent({ open, handleOpen, handleClose }) {
   return (
     <div>
       <Button onClick={handleOpen} variant="contained" color="primary">
-        Info
+        <InfoIcon />
       </Button>
       <Dialog open={open} onClose={handleClose}>
         <DialogTitle><Typography variant="infotitle">Info</Typography></DialogTitle>
         <DialogContent>
-          <Typography variant="infotext">This website uses data from the following services/apis:</Typography>
+          <Typography variant="infotext">This website uses data from the following services & APIs:</Typography>
           <ul>
             <li>
               <Typography variant="infotext"><a href="https://palvelukartta.hel.fi/en">Servicemap</a></Typography>
