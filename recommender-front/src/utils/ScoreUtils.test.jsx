8a8fffa8dee31ec43ea44a7b4bed200ba078bb6c
ruusukivi <ruusukivi@gmail.com>
2023-06-27 20:06:36 +0300

    Added new colouring

--- recommender-front/src/utils/ScoreUtils.test.jsx
+++ recommender-front/src/utils/ScoreUtils.test.jsx
@@ -27,20 +27,76 @@ describe('parseScore', () => {
 });
 
 describe('defineClass', () => {
-  it('should return correct class for scores below 0.5', () => {
-    const score = 0.3;
+  it('should return correct class for scores below 0.1', () => {
+    const score = 0.01;
 
     const result = defineClass(score);
 
-    expect(result).toEqual('custom-cluster-icon-low');
+    expect(result).toEqual('custom-cluster-icon-score01');
+  });
+
+  it('should return correct class for scores below 0.2', () => {
+    const score = 0.15;
+
+    const result = defineClass(score);
+
+    expect(result).toEqual('custom-cluster-icon-score02');
+  });
+
+  it('should return correct class for scores below 0.3', () => {
+    const score = 0.29;
+
+    const result = defineClass(score);
+
+    expect(result).toEqual('custom-cluster-icon-score03');
+  });
+
+  it('should return correct class for scores below 0.4', () => {
+    const score = 0.39;
+
+    const result = defineClass(score);
+
+    expect(result).toEqual('custom-cluster-icon-score04');
+  });
+
+  it('should return correct class for scores below 0.5', () => {
+    const score = 0.49;
+
+    const result = defineClass(score);
+
+    expect(result).toEqual('custom-cluster-icon-score05');
+  });
+
+  it('should return correct class for scores below 06', () => {
+    const score = 0.59;
+
+    const result = defineClass(score);
+
+    expect(result).toEqual('custom-cluster-icon-score06');
+  });
+
+  it('should return correct class for scores below 07', () => {
+    const score = 0.69;
+
+    const result = defineClass(score);
+
+    expect(result).toEqual('custom-cluster-icon-score07');
+  });
+
+  it('should return correct class for scores below 08', () => {
+    const score = 0.79;
+
+    const result = defineClass(score);
+
+    expect(result).toEqual('custom-cluster-icon-score08');
   });
 
   it('should return correct class for scores below 0.9', () => {
-    const score = 0.8;
+    const score = 0.89;
 
     const result = defineClass(score);
 
-    expect(result).toEqual('custom-cluster-icon-medium');
+    expect(result).toEqual('custom-cluster-icon-score09');
   });
 
   it('should return correct class for scores 0.9 and above', () => {
@@ -48,6 +104,6 @@ describe('defineClass', () => {
 
     const result = defineClass(score);
 
-    expect(result).toEqual('custom-cluster-icon-high');
+    expect(result).toEqual('custom-cluster-icon-score10');
   });
 });
