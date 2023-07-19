0be4347880051e1830da5775c4e3fe62dc0757b8
Sami Männistö <sami.mannisto1@gmail.com>
2023-07-19 16:00:41 +0300

    Times tests

--- recommender-back/src/tests/apis/times_test.py
+++ recommender-back/src/tests/apis/times_test.py
@@ -1,19 +1,24 @@
 import unittest
 from unittest.mock import patch
-from datetime import datetime, timedelta
-from datetime import datetime as dt 
+from datetime import datetime
 from src.apis.times import get_current_time
 
 
 class TimesTest(unittest.TestCase):
-    @patch('src.apis.times.datetime')
+    @patch('src.apis.times.dt.datetime')
     def test_current_time_is_returned_correctly_when_plus_is_not_set(self, mock_datetime):
-        # Set the mocked return value for datetime.now()
-        mock_datetime.now.return_value = datetime(2023, 7, 19, 10, 30)
+        mock_datetime.now.return_value = datetime(2023, 7, 19, 15, 30)
 
-        # Call the function being tested
         current_time = get_current_time()
+        expected_time = '15:30'
 
-        # Assert the result
-        expected_time = '10:30'
+        self.assertEqual(current_time, expected_time)
+
+    @patch('src.apis.times.dt.datetime')
+    def test_current_time_is_returned_correctly_when_plus_is_set(self, mock_datetime):
+        mock_datetime.now.return_value = datetime(2023, 7, 19, 15, 30)
+
+        current_time = get_current_time(plus=3)
+        expected_time = '18:30'
+
         self.assertEqual(current_time, expected_time)
