64bbc4fbbd862cb669c22d1f811e3f19d9a5a201
Sidoroff Sami L S <samsidor@cubbli22-gold.local>
2023-07-20 01:22:52 +0300

    Test weight changes

--- recommender-back/src/tests/apis/poi_test.py
+++ recommender-back/src/tests/apis/poi_test.py
@@ -24,6 +24,22 @@ class TestPointOfInterest(unittest.TestCase):
 
         self.assertAlmostEqual(score, expected_score, places=6)
 
+    def test_outdoor_score_temp_weight_change(self):
+        temperature = 7
+        wind_speed = 5
+        humidity = 0.5
+        precipitation = 20
+        clouds = 0.6
+        sunrise = datetime(2023, 6, 23, 6, 0)
+        sunset = datetime(2023, 6, 23, 18, 0)
+        cur_time = datetime(2023, 6, 23, 12, 0)
+
+        expected_score = 0.32
+
+        score = self.poi._outdoor_score(temperature, wind_speed, humidity, precipitation, clouds, sunrise, sunset, cur_time)
+
+        self.assertAlmostEqual(score, expected_score, places=6)
+
     def test_indoor_score(self):
         temperature = 22
         wind_speed = 2
@@ -33,18 +49,34 @@ class TestPointOfInterest(unittest.TestCase):
         sunrise = datetime(2023, 6, 23, 6, 0)
         sunset = datetime(2023, 6, 23, 18, 0)
         cur_time = datetime(2023, 6, 23, 20, 0)
-        
+
         expected_score = 0.81
 
         score = self.poi._indoor_score(temperature, wind_speed, humidity, precipitation, clouds, sunrise, sunset, cur_time)
 
         self.assertAlmostEqual(score, expected_score, places=6)
-        
+
+    def test_indoor_score_temp_weight_change(self):
+        temperature = 7
+        wind_speed = 2
+        humidity = 0.45
+        precipitation = 200
+        clouds = 0.8
+        sunrise = datetime(2023, 6, 23, 6, 0)
+        sunset = datetime(2023, 6, 23, 18, 0)
+        cur_time = datetime(2023, 6, 23, 20, 0)
+
+        expected_score = 0.85
+
+        score = self.poi._indoor_score(temperature, wind_speed, humidity, precipitation, clouds, sunrise, sunset, cur_time)
+
+        self.assertAlmostEqual(score, expected_score, places=6)
+
     def test_calculate_score_chooses_indoor(self):
         with patch('src.apis.times.get_current_time') as mock_get_current_time:
             weather = {'20:00':{'Wind speed':'2.0 m/s','Precipitation':'200 mm','Cloud amount':'0.6 %',
                     'Air temperature':'22.0 *C','Humidity':'0.45 %'}}
-            
+
             sun = ('06:00','18:00')
             mock_get_current_time.return_value = '20:00'
             categories = ['Sport halls']
@@ -52,19 +84,19 @@ class TestPointOfInterest(unittest.TestCase):
             test_poi = PointOfInterest(categories=categories)
             test_poi.weather = weather
             test_poi.sun = sun
-            
+
             expected_score = 0.79
 
             test_poi.calculate_score()
             score = test_poi.weather['20:00']['Score']
 
             self.assertAlmostEqual(score, expected_score, places=6)
-            
+
     def test_calculate_score_chooses_outdoor(self):
         with patch('src.apis.times.get_current_time') as mock_get_current_time:
             weather = {'12:00':{'Wind speed':'5.0 m/s','Precipitation':'20 mm','Cloud amount':'0.6 %',
                     'Air temperature':'23.0 *C','Humidity':'0.5 %'}}
-            
+
             sun = ('06:00','18:00')
             mock_get_current_time.return_value = '12:00'
             categories = ['Open air pools and beaches']
@@ -72,7 +104,7 @@ class TestPointOfInterest(unittest.TestCase):
             test_poi = PointOfInterest(categories=categories)
             test_poi.weather = weather
             test_poi.sun = sun
-            
+
             expected_score = 0.54
 
             test_poi.calculate_score()
