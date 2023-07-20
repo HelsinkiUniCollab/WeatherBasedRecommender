fc50b9d03a04b7d3d511d9c36eb80ae52e727f5d
saavajuu <juuso.saavalainen@helsinki.fi>
2023-07-20 11:26:42 +0300

    added setup

--- recommender-back/src/tests/apis/manager_test.py
+++ recommender-back/src/tests/apis/manager_test.py
@@ -4,37 +4,8 @@ from src.apis.poi import PointOfInterest
 
 
 class TestManger(unittest.TestCase):
-    def test_get_pois_returns_list(self):
-        result = get_pois()
-        self.assertIsInstance(result, list)
-
-    def test_get_pois_contains_items(self):
-        result = get_pois()
-        self.assertTrue(len(result) > 0)
-
-    def test_get_pois_all_items_are_point_of_interest_objects(self):
-        result = get_pois()
-        for poi in result:
-            self.assertIsInstance(poi, PointOfInterest)
-
-    def test_get_pois_structure(self):
-        result = get_pois()
-        for poi in result:
-            self.assertTrue(hasattr(poi, 'name'))
-            self.assertTrue(hasattr(poi, 'latitude'))
-            self.assertTrue(hasattr(poi, 'longitude'))
-            self.assertTrue(hasattr(poi, 'not_accessible_for'))
-            self.assertTrue(hasattr(poi, 'categories'))
-
-    def test_get_pois_coordinates_valid(self):
-        result = get_pois()
-        for poi in result:
-            self.assertIsInstance(poi.latitude, float)
-            self.assertIsInstance(poi.longitude, float)
-
-    def test_find_nearest_coordinates(self):
-        pois = get_pois()
-        fore = {
+    def setUp(self):
+        self.fore = {
         "2023-07-20 12:00:00": {
         "60.201231, 24.973478": {
         "Air temperature": "20.5 °C",
@@ -105,12 +76,41 @@ class TestManger(unittest.TestCase):
         }
         }
         }
-        for i in pois:
-            a = find_nearest_coordinate_forecast_data(i,fore)
-            self.assertEqual(len(a.weather),3)
+
+    def test_get_pois_returns_list(self):
+        result = get_pois()
+        self.assertIsInstance(result, list)
+
+    def test_get_pois_contains_items(self):
+        result = get_pois()
+        self.assertTrue(len(result) > 0)
+
+    def test_get_pois_all_items_are_point_of_interest_objects(self):
+        result = get_pois()
+        for poi in result:
+            self.assertIsInstance(poi, PointOfInterest)
+
+    def test_get_pois_structure(self):
+        result = get_pois()
+        for poi in result:
+            self.assertTrue(hasattr(poi, 'name'))
+            self.assertTrue(hasattr(poi, 'latitude'))
+            self.assertTrue(hasattr(poi, 'longitude'))
+            self.assertTrue(hasattr(poi, 'not_accessible_for'))
+            self.assertTrue(hasattr(poi, 'categories'))
+
+    def test_get_pois_coordinates_valid(self):
+        result = get_pois()
+        for poi in result:
+            self.assertIsInstance(poi.latitude, float)
+            self.assertIsInstance(poi.longitude, float)
+
+    def test_find_nearest_coordinates(self):
+        pois = get_pois()
+        for onepoi in pois:
+            res = find_nearest_coordinate_forecast_data(onepoi,self.fore)
+            self.assertEqual(len(res.weather),3)
             break
-
-
 
 if __name__ == '__main__':
     unittest.main()
