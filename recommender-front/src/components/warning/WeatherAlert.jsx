790739df1cc1e652b4cb8bafe70a52c7aa79f196
saavajuu <juuso.saavalainen@helsinki.fi>
2023-07-20 13:48:16 +0300

    added bigger warning on top of the map

--- recommender-front/src/components/warning/WeatherAlert.jsx
+++ recommender-front/src/components/warning/WeatherAlert.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import Alert from '@mui/material/Alert';
 
+const fullscreenAlertStyle = {
+  position: 'absolute',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  zIndex: 9999,
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  backgroundColor: 'rgba(0, 0, 0, 0.5)', // You can adjust the background color and transparency here
+};
+
 function WeatherAlert({ showAlert }) {
   if (!showAlert) {
     return null;
   }
 
   return (
-    <Alert severity="warning" sx={{ marginBottom: '5px' }}>
-      You should avoid going outside due to strong wind.
-      We do not provide any recommendations and all the controls are disabled.
-    </Alert>
+    <div style={fullscreenAlertStyle}>
+      <Alert severity="warning" sx={{ fontSize: '80px', color: '#000' }}>
+        You should avoid going outside due to strong wind.
+        We do not provide any recommendations and all the controls are disabled.
+      </Alert>
+    </div>
   );
 }
 
