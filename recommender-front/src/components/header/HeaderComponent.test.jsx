7962132c4b0cc67645a272a2e8eee5591af567d7
ruusukivi <ruusukivi@gmail.com>
2023-07-20 15:00:38 +0300

    Added tests

--- recommender-front/src/components/header/HeaderComponent.test.jsx
+++ recommender-front/src/components/header/HeaderComponent.test.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import HeaderComponent from './HeaderComponent';
+import LoadingIndicatorComponent from './LoadingIndicatorComponent';
 
 jest.mock('react-leaflet', () => {
   const originalModule = jest.requireActual('react-leaflet');
@@ -11,7 +12,7 @@ jest.mock('react-leaflet', () => {
 });
 
 describe('HeaderComponent', () => {
-  test('renders without crashing', () => {
+  test('renders without crashing without poiData', () => {
     const mockProps = {
       accessibility: 'wheelchair',
       handleChange: jest.fn(),
@@ -22,6 +23,22 @@ describe('HeaderComponent', () => {
       handleOpen: jest.fn(),
       handleClose: jest.fn(),
       isMobile: false,
+      poiData: [],
+    };
+    // eslint-disable-next-line react/jsx-props-no-spreading
+    render(<HeaderComponent {...mockProps} />);
+  });
+  test('renders without crashing in Mobile', () => {
+    const mockProps = {
+      accessibility: 'wheelchair',
+      handleChange: jest.fn(),
+      times: [],
+      sliderValue: 0,
+      onChange: jest.fn(),
+      open: false,
+      handleOpen: jest.fn(),
+      handleClose: jest.fn(),
+      isMobile: true,
       poiData: [],
     };
     // eslint-disable-next-line react/jsx-props-no-spreading
