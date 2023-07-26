f98a68c0ad616c51a7925194bbd15b87076c0d2f
Joni Joensuu <joni.joens@gmail.com>
2023-07-26 21:20:45 +0300

    working backend refactor

--- recommender-back/src/apis/forecast.py
+++ recommender-back/src/apis/forecast.py
@@ -1,11 +1,14 @@
 import math
+import time
+import errno
 import numpy as np
 from .times import utc_to_finnish, get_forecast_times
 from ..config import Config
-from ..services.forecastdatafetcher import ForecastDataFetcher
+from ..services.forecastdatafetcher import DataFetcher
+
 
 class Forecast:
-    def __init__(self, fetcher: ForecastDataFetcher):
+    def __init__(self, fetcher: DataFetcher):
         self.fetcher = fetcher
         self.data = None
         self.valid_times = None
@@ -13,66 +16,113 @@ class Forecast:
         self.coordinates = None
 
     def update_data(self):
-        '''
-        Updates the forecast data by fetching the latest data from the API.
-        '''
+        """
+        Updates the forecast data.
+        Fetches the latest data and updates class properties if new data is available.
+        """
         current, start, end = get_forecast_times()
-        print(f'Query for the new Grid object at time: {current} UTC')
-        forecast_data = self.fetcher.get_forecast_data(
-            start,
-            end,
-            Config.BBOX,
-            Config.TIMESTEP,
-            Config.PARAMETERS
-            )
-
+        print(f"Query for the new Grid object at time: {current} UTC")
+        forecast_data = self.get_latest_forecast(start, end)
         latest_forecast = max(forecast_data.data.keys())
-        self.data = forecast_data.data[latest_forecast]
-        self.data.parse(delete=True)
+
+        if not self.data or latest_forecast > max(self.data.keys()):
+            self.data = forecast_data.data[latest_forecast]
+            self.parse_forecast_data()
+            self.update_forecast_properties()
+
+    def get_latest_forecast(self, start, end):
+        """
+        Retrieves the latest forecast data within the specified time range.
+        Args:
+            start (datetime): The start time for the data retrieval.
+            end (datetime): The end time for the data retrieval.
+        Returns:
+            Grid: The forecast grid data within the specified time range.
+        """
+        return self.fetcher.get_forecast_data(
+            start, end, Config.BBOX, Config.TIMESTEP, Config.PARAMETERS
+        )
+
+    def parse_forecast_data(self):
+        """
+        Parses the forecast data with retry attempts on failure.
+        Raises:
+            ConnectionResetError: If a ConnectionResetError occurs during parsing after maximum number of retry attempts.
+        """
+        max_retries = 3
+        retry_delay = 5  # delay in seconds
+
+        for attempt in range(max_retries):
+            try:
+                self.data.parse(delete=True)
+                break
+            except ConnectionResetError as error:
+                if error.errno != errno.ECONNRESET:
+                    # Not the error we are looking for, re-raise
+                    raise
+                print(f"ConnectionResetError during parsing: {error}")
+                if attempt < max_retries - 1:
+                    time.sleep(retry_delay)
+                    print(f"Retrying parsing ({attempt + 1} out of {max_retries})...")
+                else:
+                    print(
+                        f"Parsing failed after {max_retries} attempts due to ConnectionResetError."
+                    )
+                    raise
+
+    def update_forecast_properties(self):
+        """
+        Updates forecast properties based on the latest parsed data.
+        """
         self.valid_times = self.data.data.keys()
         earliest_step = min(self.valid_times)
         self.data_levels = self.data.data[earliest_step].keys()
-        self.coordinates = np.dstack(
-            (self.data.latitudes, self.data.longitudes))
+        self.coordinates = np.dstack((self.data.latitudes, self.data.longitudes))
+        self.coordinates = np.dstack((self.data.latitudes, self.data.longitudes))
 
     def get_data(self):
-        '''
+        """
         Gets all the forecast data from the grid.
 
         Returns:
             dict: A dictionary containing all the forecast data.
-        '''
+        """
         data = {}
         for date_time in self.valid_times:
             local_time = utc_to_finnish(date_time)
-            time_str = local_time.strftime('%Y-%m-%d %H:%M:%S')
+            time_str = local_time.strftime("%Y-%m-%d %H:%M:%S")
             coordinates_data = {}
             for level in self.data_levels:
                 datasets = self.data.data[date_time][level]
                 for dataset_name, dataset in datasets.items():
-                    unit = dataset['units']
-                    data_array = dataset['data']
-                    for (lat_index, lon_index), data_value in np.ndenumerate(data_array):
+                    unit = dataset["units"]
+                    data_array = dataset["data"]
+                    for (lat_index, lon_index), data_value in np.ndenumerate(
+                        data_array
+                    ):
                         latitude = self.coordinates[lat_index, lon_index, 0]
                         longitude = self.coordinates[lat_index, lon_index, 1]
                         key = str((latitude, longitude))
                         if key not in coordinates_data:
                             coordinates_data[key] = []
                         coordinates_data[key].append(
-                            {'Dataset': dataset_name, 'Unit': unit, 'Data': data_value})
+                            {"Dataset": dataset_name, "Unit": unit, "Data": data_value}
+                        )
             data[time_str] = coordinates_data
         return data
 
     def get_coordinates(self):
-        '''
+        """
         Returns all available coordinates within the bounding box area.
 
         Returns:
             list: List of coordinate pairs.
-        '''
+        """
         unique_coords = set()
 
-        flattened_coords = [tuple(coord) for sublist in self.coordinates for coord in sublist]
+        flattened_coords = [
+            tuple(coord) for sublist in self.coordinates for coord in sublist
+        ]
 
         for coord in flattened_coords:
             unique_coords.add(coord)
@@ -80,7 +130,7 @@ class Forecast:
         return list(unique_coords)
 
     def get_closest_poi_coordinates_data(self, pois):
-        '''
+        """
         Finds the nearest coordinates forecast data for all of the POI's coordinates.
 
         Args:
@@ -88,13 +138,13 @@ class Forecast:
 
         Returns:
             dict: A dictionary containing the nearest coordinates forecast data for each hour.
-        '''
+        """
         data = self.get_data()
         coordinates = self.get_coordinates()
         returned_data = {hour: {} for hour in data}
         closest_coordinates = {}
         for poi in pois:
-            smallest = float('inf')
+            smallest = float("inf")
             nearest = []
             lat = poi.latitude
             lon = poi.longitude
@@ -104,17 +154,19 @@ class Forecast:
                     smallest = dist
                     nearest = [coordinate[0], coordinate[1]]
             closest_coordinates[(lat, lon)] = nearest
-            
+
         for hour, hour_data in data.items():
             for poi_coord, nearest in closest_coordinates.items():
-                nearest_str = f'({nearest[0]}, {nearest[1]})'
+                nearest_str = f"({nearest[0]}, {nearest[1]})"
                 if nearest_str in hour_data:
                     forecast = hour_data[nearest_str]
-                    returned_data[hour][f'{poi_coord[0]}, {poi_coord[1]}'] = self.parse_forecast(forecast)
+                    returned_data[hour][
+                        f"{poi_coord[0]}, {poi_coord[1]}"
+                    ] = self.parse_forecast(forecast)
         return returned_data
 
     def parse_forecast(self, forecast):
-        '''
+        """
         Parses the desired data from the forecast grid.
 
         Args:
@@ -122,34 +174,32 @@ class Forecast:
 
         Returns:
             dict: A dictionary containing the parsed forecast data.
-        '''
+        """
         for value in forecast:
+            if value["Dataset"] == "2 metre temperature":
+                temperature = round(value["Data"] - 273.15, 1)
+            elif value["Dataset"] == "2 metre relative humidity":
+                humidity = round(value["Data"], 1)
+            elif value["Dataset"] == "10 metre U wind component":
+                u_wind = value["Data"]
+            elif value["Dataset"] == "10 metre V wind component":
+                v_wind = value["Data"]
+            elif value["Dataset"] == "surface precipitation amount, rain, convective":
+                precipitation = round(value["Data"], 1)
+            elif value["Dataset"] == "Total Cloud Cover":
+                cloudcoverage = round(value["Data"], 1)
 
-            if value['Dataset'] == '2 metre temperature':
-                temperature = round(value['Data'] - 273.15, 1)
-            elif value['Dataset'] == '2 metre relative humidity':
-                humidity = round(value['Data'], 1)
-            elif value['Dataset'] == '10 metre U wind component':
-                u_wind = value['Data']
-            elif value['Dataset'] == '10 metre V wind component':
-                v_wind = value['Data']
-            elif value['Dataset'] == 'surface precipitation amount, rain, convective':
-                precipitation = round(value['Data'], 1)
-            elif value['Dataset'] == 'Total Cloud Cover':
-                cloudcoverage = round(value['Data'], 1)
-
-        wind_speed = self.calculate_wind_speed_and_direction(
-            u_wind, v_wind)
+        wind_speed = self.calculate_wind_speed_and_direction(u_wind, v_wind)
         return {
-            'Air temperature': f'{str(temperature)} °C',
-            'Humidity': f'{str(humidity)} %',
-            'Wind speed': f'{wind_speed} m/s',
-            'Precipitation': f'{precipitation} mm',
-            'Cloud amount': f'{cloudcoverage} %',
+            "Air temperature": f"{str(temperature)} °C",
+            "Humidity": f"{str(humidity)} %",
+            "Wind speed": f"{wind_speed} m/s",
+            "Precipitation": f"{precipitation} mm",
+            "Cloud amount": f"{cloudcoverage} %",
         }
 
     def calculate_wind_speed_and_direction(self, u_wind, v_wind):
-        '''
+        """
         Calculates the wind speed and direction based on the U and V components.
 
         Args:
@@ -158,7 +208,7 @@ class Forecast:
 
         Returns:
             tuple: A tuple containing the wind speed and direction.
-        '''
+        """
         wind_speed = math.sqrt(u_wind**2 + v_wind**2)
         wind_direction = math.atan2(u_wind, v_wind) * (180 / math.pi)
         wind_direction = (wind_direction + 360) % 360
--- recommender-back/src/tests/services/forecastdatafetcher_test.py
+++ recommender-back/src/tests/services/forecastdatafetcher_test.py
@@ -1,38 +1,46 @@
 from unittest import TestCase
 from unittest.mock import patch
-from src.services.forecastdatafetcher import ForecastDataFetcher
+from src.services.forecastdatafetcher import DataFetcher
 
-class TestForecastDataFetcher(TestCase):
+
+class TestDataFetcher(TestCase):
     def setUp(self):
-        self.fetcher = ForecastDataFetcher()
-        self.start = 'start'
-        self.end = 'end'
-        self.bbox = 'bbox'
-        self.timestep = 'timestep'
-        self.parameters = 'parameters'
-        self.timeseries = 'timeseries'
+        self.fetcher = DataFetcher()
+        self.start = "start"
+        self.end = "end"
+        self.bbox = "bbox"
+        self.timestep = "timestep"
+        self.parameters = "parameters"
+        self.timeseries = "timeseries"
 
-    @patch('src.services.forecastdatafetcher.download_stored_query')
+    @patch("src.services.forecastdatafetcher.download_stored_query")
     def test_get_forecast_data(self, mock_download):
-        mock_download.return_value = 'mock forecast data'
-        
-        result = self.fetcher.get_forecast_data(self.start, self.end, self.bbox, self.timestep, self.parameters)
+        mock_download.return_value = "mock forecast data"
 
-        mock_download.assert_called_once_with('fmi::forecast::harmonie::surface::grid',
-                                              args=[f'starttime={self.start}',
-                                                    f'endtime={self.end}',
-                                                    f'bbox={self.bbox}',
-                                                    f'timestep={self.timestep}',
-                                                    f'parameters={self.parameters}'])
-        self.assertEqual(result, 'mock forecast data')
+        result = self.fetcher.get_forecast_data(
+            self.start, self.end, self.bbox, self.timestep, self.parameters
+        )
 
-    @patch('src.services.forecastdatafetcher.download_stored_query')
+        mock_download.assert_called_once_with(
+            "fmi::forecast::harmonie::surface::grid",
+            args=[
+                f"starttime={self.start}",
+                f"endtime={self.end}",
+                f"bbox={self.bbox}",
+                f"timestep={self.timestep}",
+                f"parameters={self.parameters}",
+            ],
+        )
+        self.assertEqual(result, "mock forecast data")
+
+    @patch("src.services.forecastdatafetcher.download_stored_query")
     def test_get_current_weather_data(self, mock_download):
-        mock_download.return_value = 'mock weather data'
-        
+        mock_download.return_value = "mock weather data"
+
         result = self.fetcher.get_current_weather_data(self.bbox, self.timeseries)
 
-        mock_download.assert_called_once_with('fmi::observations::weather::multipointcoverage',
-                                              args=[f'bbox={self.bbox}',
-                                                    f'timeseries={self.timeseries}'])
-        self.assertEqual(result, 'mock weather data')
+        mock_download.assert_called_once_with(
+            "fmi::observations::weather::multipointcoverage",
+            args=[f"bbox={self.bbox}", f"timeseries={self.timeseries}"],
+        )
+        self.assertEqual(result, "mock weather data")
