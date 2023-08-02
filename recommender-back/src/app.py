589244afabd28e0ddfda722a1e80ecaef87af0dd
Joni Joensuu <joni.joens@gmail.com>
2023-08-02 15:19:07 +0300

    redis cache working

--- recommender-back/src/app.py
+++ recommender-back/src/app.py
@@ -2,19 +2,20 @@ from flask import Flask
 from flask_caching import Cache
 from flask_cors import CORS
 from dotenv import load_dotenv
+from .config import Config
 
 load_dotenv()
 
 config = {
     "DEBUG": True,
-    "CACHE_TYPE": "SimpleCache",
+    "CACHE_TYPE": Config.CACHE_TYPE,
+    "CACHE_REDIS_URL": Config.CACHE_REDIS_URL,
     "CACHE_DEFAULT_TIMEOUT": 300
 }
 
 app = Flask(__name__)
 CORS(app)
 
-cache = Cache(config={'CACHE_TYPE': 'SimpleCache'})
-cache.init_app(app)
+cache = Cache(app, config=config)
 
 from src import routes
