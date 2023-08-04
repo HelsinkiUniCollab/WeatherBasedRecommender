9156099a7a4a0ab8906e0f00a7039cceeea53f78
Joni Joensuu <joni.joens@gmail.com>
2023-08-04 11:50:08 +0300

    test for simulatorformcomponent

---
+++ recommender-front/src/components/simulator/SimulatorFormComponent.test.jsx
@@ -0,0 +1,15 @@
+import { render, fireEvent } from '@testing-library/react';
+import React from 'react';
+import SimulatorFormComponent from './SimulatorFormComponent';
+
+test('renders the SimulatorFormComponent and checks form interactions', () => {
+  const mockHandleInputChange = jest.fn();
+  const { getByLabelText } = render(<SimulatorFormComponent
+    handleInputChange={mockHandleInputChange}
+  />);
+
+  const airTemperatureInput = getByLabelText('Air Temperature');
+  fireEvent.change(airTemperatureInput, { target: { value: '20' } });
+
+  expect(mockHandleInputChange).toHaveBeenCalled();
+});
