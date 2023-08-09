ce0364860980b87934019e66fc5508f6ab0572d2
Sami Männistö <sami.mannisto1@gmail.com>
2023-08-09 10:45:48 +0300

    Resolve conflicts

--- recommender-back/src/config.py
+++ recommender-back/src/config.py
@@ -1,14 +1,11 @@
 class Config:
     BBOX = '24.5,60,25.5,60.5'
     TIMESTEP = 60
-<<<<<<< HEAD
     PARAMETERS = 'Temperature,Humidity,WindUMS,WindVMS,PrecipitationAmount,TotalCloudCover'
     AQI_PARAMS = 'AQIndex'
     FMI_QUERY_URL = 'http://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id='
     AQI_QUERY = 'fmi::forecast::enfuser::airquality::helsinki-metropolitan::grid'
     FILEREF_MEMBER = './/{http://www.opengis.net/gml/3.2}fileReference'
-=======
     FORECAST_PARAMETERS = 'Temperature,Humidity,WindUMS,WindVMS,PrecipitationAmount,TotalCloudCover'
     AIRQUALITY_PARAMETERS = 'AQINDEX_PT1H_avg'
     CACHE_REDIS_URL = 'redis://redis:6379/0'
->>>>>>> main
