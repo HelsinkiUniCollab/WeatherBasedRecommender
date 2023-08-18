26e24ae4f2ecf678a9a6f9536894508ed649df11
Sami Männistö <sami.mannisto1@gmail.com>
2023-08-18 16:55:39 +0300

    Fix default cache timing

--- recommender-back/src/app.py
+++ recommender-back/src/app.py
@@ -16,7 +16,7 @@ config = {
     "DEBUG": True,
     "CACHE_TYPE": CACHE_TYPE,
     "CACHE_REDIS_URL": Config.CACHE_REDIS_URL,
-    "CACHE_DEFAULT_TIMEOUT": 180
+    "CACHE_DEFAULT_TIMEOUT": 3600
 }
 
 app = Flask(__name__)
