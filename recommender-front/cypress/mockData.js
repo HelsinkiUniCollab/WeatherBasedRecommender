ede3f3da994c32283e43878d758c0869df069498
Joni Joensuu <joni.joens@gmail.com>
2023-08-19 18:35:05 +0300

    first commit, mockData append

--- recommender-front/cypress/mockData.js
+++ recommender-front/cypress/mockData.js
@@ -56,6 +56,37 @@ const mockPOIs = [
       },
     },
     category: 'Open air pools and beaches',
+    not_accessible_for: [],
+  },
+  {
+    name: 'Mock POI 3',
+    latitude: 60.180,
+    longitude: 24.876,
+    weather: {
+      Current: {
+        'Air temperature': '20.0 °C',
+        Wind: '5.0 m/s',
+        'Air pressure': '1019 mbar',
+        Humidity: '45.0 %',
+        Score: 1.0,
+      },
+      '01:00': {
+        'Air temperature': '-5.5 °C',
+        Wind: '3.4 m/s',
+        'Air pressure': '1018 mbar',
+        Humidity: '43.3 %',
+        Score: 0.0,
+      },
+      '19:00': {
+        'Air temperature': '20.7 °C',
+        Wind: '4.7 m/s',
+        'Air pressure': '1019 mbar',
+        Humidity: '44.7 %',
+        Score: 0.3,
+      },
+    },
+    category: 'Fitness training parks',
+    not_accessible_for: [],
   },
 ];
 
